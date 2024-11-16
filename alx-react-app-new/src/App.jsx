3ee2a84f095bbbd8efa407136c4d7c2b46e0fdb4
badawaJM <badawamagnetena666@gmail.com>
2024-11-16 17:27:45 +0000

    Counter app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 function App() {
   
   return (
     <>
 
       <div>
-      <WelcomeMessage />
-    <Header />
-    < MainContent />
-    <Footer/>
-    </div>
+       <WelcomeMessage />
+       <Header />
+       <MainContent />
+       <Footer/>
+       <Header />
+       <Counter/>
+      </div>
     </>
   )
 }
