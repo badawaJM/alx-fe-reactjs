435bd7708469a1eee8b582db921f708c1ee0d74c
badawaJM <badawamagnetena666@gmail.com>
2024-12-15 18:08:09 +0000

    Integrating the GitHub API and Creating a Basic Search Feature

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Cible tous les fichiers .js, .jsx, .ts, .tsx dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: false,
+};
